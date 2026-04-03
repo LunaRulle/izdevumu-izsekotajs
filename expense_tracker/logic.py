e9836e9c69f968f9d84abce8737596c0fe5bf06b
Luna Rulle <luna.rulle@pm.me>
2026-04-03 15:42:01 +0300

    feat: added get_available_months function

--- expense_tracker/logic.py
+++ expense_tracker/logic.py
@@ -5,4 +5,21 @@ def sum_total(expenses):
     for exp in expenses: #type: ignore
         total = exp["sum"] + total
     return total
-        
+       
+def sum_by_category(expenses):
+    totals = {}
+    for exp in expenses: 
+        cat = exp["category"]
+        totals[cat] = totals.get(cat, 0) + exp["sum"]
+    return {cat: total for cat, total in totals.items()}
+
+def get_available_months(expenses):
+    months = []
+    for exp in expenses:
+        dates = datetime.strptime(exp["date"], "%Y-%m-%d")
+        month = datetime.strftime(dates, "%Y-%m")
+        if month not in months:
+            months.append(month)
+    return months
+
+
